5197f4964106ae74e6e1c50041bdb9bd416d3b39
Michael Clermont <michael.clermont@mths.ca>
2022-09-20 01:49:15 +0000

    edited weight program

--- Main.java
+++ Main.java
@@ -8,11 +8,11 @@
 
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static final int LogWeight = 20;
     public static final int TruckCarryLimit = 1100;
 
-    static void Main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("What is the length of the log (0.25, 0.5 or 1): "); 
