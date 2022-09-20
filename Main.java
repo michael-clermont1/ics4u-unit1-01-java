4f4f9d7dcedbd161cf297d926267c244395cee79
Michael Clermont <michael.clermont@mths.ca>
2022-09-20 01:38:52 +0000

    edited weight program

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
-/**
+/*
  * The program calculates the amount of logs
  *
- * By:      Michael Clermontl
- * Version: 1.0
- * Since:   2022-11-25
- */
+ * @author:  Michael Clermontl
+ * @version: 1.0
+ * @since:   2022-11-25
+*/
 
 import java.util.Scanner;
 
-class main {
+class Main {
     public static final int LogWeight = 20;
     public static final int TruckCarryLimit = 1100;
 
-    public static void main(String[] args) {
+    static void Main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("What is the length of the log (0.25, 0.5 or 1): "); 
